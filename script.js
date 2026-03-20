136190383264045552bcb27f023f9dbbdf153671
NickMakleury <nick.almeida@uniaene.edu.br>
2026-03-19 23:35:13 -0300

    resposividade no head

--- script.js
+++ script.js
@@ -73,3 +73,13 @@ counters.forEach(counter => {
     updateCount();
 
 });
+
+const topbar = document.querySelector('.topbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 40) {
+    topbar.classList.add('scrolled');
+  } else {
+    topbar.classList.remove('scrolled');
+  }
+});
